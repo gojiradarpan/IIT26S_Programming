c7b66e10295fde7940c6ba15d54a7d5cc44e9622
Darpan Bhandari <darpanbh25@gmail.com>
2026-09-14 16:06:15 +0300

    T5 done

---
+++ W1/A1_T5.py
@@ -0,0 +1,8 @@
+print("Calculate the area of a wall.")
+Feed = input("Enter the width in metres: ")
+Width = int(Feed)
+Feed = input("Enter the height in metres: ")
+Height = int(Feed)
+print(f"Width is {Width} m and height is {Height} m.")
+Area = Width * Height
+print(f"The wall will be {Area} square metres.")
