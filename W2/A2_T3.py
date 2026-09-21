33fe688c2e83b22050ee61425369419c070adf65
Darpan Bhandari <darpanbh25@gmail.com>
2026-09-21 09:38:05 +0300

    update

--- W2/A2_T3.py
+++ W2/A2_T3.py
@@ -18,5 +18,5 @@ word2 = input("Insert second word: ")
 
 print(f"1st word is {len(word1)} characters long.")
 print(f"2nd word is {len(word2)} characters long.")
-print(f"Words together makes one closed compound '{word1}{word2}'")
+print(f"Words together makes one closed compound '{word1}{word2}'.")
 print("Program ending.")
---
+++ W2/A2_T4.py
@@ -0,0 +1,22 @@
+# Prompt the user to insert the minutes spent on each previous topic task.
+# Calculate the sum and the average. Display those in the example program run format(Note! precision).
+# Make sure to calculate the required average for two decimal digits and later integer as rounded integer (precision 0 + type conversion).
+
+# Example program run:
+
+# Program starting.
+# Estimate how many minutes you spent on programming...
+
+# A1_T1: 3
+# A1_T2: 7
+# A1_T3: 9
+# A1_T4: 8
+# A1_T5: 13
+# A1_T6: 14
+# A1_T7: 21
+
+# In total you spent 75 minutes on programming.
+# Average per task was 10.71 min and same rounded to the nearest integer 11 min.
+
+# Program ending.
+
