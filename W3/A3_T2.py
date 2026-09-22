6a8fb05c8d114cfae9f4611c91391baa5ddc0e99
Darpan Bhandari <darpanbh25@gmail.com>
2026-09-22 20:50:20 +0300

    tf is wrong with this code

---
+++ W3/A3_T2.py
@@ -0,0 +1,15 @@
+print("Program staring.\nString comparisions")
+word = input("Insert first word: ").lower()
+character = input("Insert a character: ").lower()
+if character in word:
+    print(f"Word \"{word}\" contains character \"{character}\")
+else:
+    print(f"Word \"{word}\") doesn't contain character \"{character}\")
+word2 = input("Insert second word: ").lower()
+if word == word1:
+    print(f"Both inserrted words are the same alphabetically, \"{word}"\")
+elif word < word2:
+    print(f"The first word \"{word}\" is before the second word \"{word2}\" alphabetically.")
+else:
+    print(f"The second word \"{word2}\" is before the first word \"{word}\ alphabetically.")
+print("Program ending.")
